a8b4edb78c135758dbbf05c888b85e622eefa967
GoTo15 <goto15pod@gmail.com>
2021-02-07 13:16:29 -0600

    Finally figure out how to use translate with push and pop

--- Tiles/truchetTiles.js
+++ Tiles/truchetTiles.js
@@ -2,22 +2,19 @@ function randIntBetween(min, max) {
   return Math.floor(Math.random() * (Math.floor(max) - Math.ceil(min)) + Math.floor(min));
 }
 
-const TruchetTiles = {
+class TruchetTiles {
   tileZero(x, y){
     arc(x, y, 25, 25, 0, Math.PI/2);
     arc(x + 25, y + 25, 25, 25, Math.PI, 1.5 * Math.PI);
-  },
+  }
+
   tileOne(x, y){
     arc(x, y+25, 25, 25, 1.5*Math.PI, 2*Math.PI);
     arc(x+25, y, 25, 25, Math.PI/2, Math.PI);
-  },
+  }
+
   chooseTile(x, y){
     let tileType = randIntBetween(0, 2);
-
-    if(tileType === 0){
-      TruchetTiles.tileZero(x, y);
-    } else {
-      TruchetTiles.tileOne(x, y);
-    }
+    tileType === 0 ? this.tileZero(x, y) : this.tileOne(x, y);
   }
 }
--- sketch.js
+++ sketch.js
@@ -1,15 +1,27 @@
-let canvasX = 100;
-let canvasY = 100;
+let canvasX = 300;
+let canvasY = 600;
 
 function setup(){
   createCanvas(canvasX, canvasY, SVG);
   noFill();
 }
 
+function randIntBetween(min, max) {
+  return Math.floor(Math.random() * (Math.floor(max) - Math.ceil(min)) + Math.floor(min));
+}
+
 function draw(){
-  for(let i = 0; i < canvasX; i += 25){
-    for(let j = 0; j < canvasY; j += 25){
+  let padding = 4;
+  let width = 16;
+
+  for(let i = 2; i < canvasX; i += 20){
+    push(); // Start translation
+    translate(i, 0);
+    for(let j = 0; j < canvasY; j += 20 + padding){
+      translate(0, width + padding);
+      square(0, 0, width);
     }
+    pop();  // End translation
   }
 
   noLoop();
